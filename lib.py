342e972ba94e9064397494063529aad6de31f6fa
hidekekw <caioshimohiro@hotmail.com>
2024-12-11 10:59:13 -0300

    Mutacao correta

--- lib.py
+++ lib.py
@@ -11,9 +11,11 @@ class individual:
         self.fit = fitFunc(self.real_x, self.real_y)
 
 class population:
-    def __init__(self, pop_size):
+    def __init__(self, pop_size, cross_rate, mutation_rate):
         self.pop_size = pop_size
         self.generation = 1
+        self.mutation_rate = mutation_rate*100 # Para realizar operacoes com inteiros ao inves de float
+        self.cross_rate = cross_rate*100       # Idem
         self.individuals = self.initFirstGen()
     
     def initFirstGen(self):
@@ -52,6 +54,58 @@ class population:
         filename = "./graficos/gen"+str(self.generation)+".png"
         plt.savefig(filename)
 
+    def cross(self, par_1_index, par_2_index):
+
+        def bound_check(bin_gene):
+            real_gene = binaryToReal(bin_gene)
+            if real_gene >= -10 and real_gene <= 10:
+                return True
+            return False
+        
+        def mutation(original_gene, mut_rate):
+            mut_gene = []
+            for bit in original_gene:
+                if random.randint(0, 100) < mut_rate:
+                    if bit == "1":
+                        mut_gene.append("0")
+                    else:
+                        mut_gene.append("1")
+                else:
+                    mut_gene.append(bit)
+            mut_gene_bin = ''.join(mut_gene)
+            bound_flag = bound_check(mut_gene_bin)
+            if bound_flag:
+                return mut_gene_bin
+            else:
+                return mut_gene_bin[0]+"10011100010000"
+
+        par_1 = self.individuals[par_1_index]
+        par_2 = self.individuals[par_2_index]
+
+        if random.randint(0, 100) >= self.cross_rate: # Define se o cruzamento ocorre. Se nao, retorna os pais
+            return par_1, par_2
+
+        cut = random.randint(1, 13)
+        par_1_bin_x = [par_1.bin_x[:cut], par_1.bin_x[cut:]]
+        par_1_bin_y = [par_1.bin_y[:cut], par_1.bin_y[cut:]]
+        par_2_bin_x = [par_2.bin_x[:cut], par_2.bin_x[cut:]]
+        par_2_bin_y = [par_2.bin_y[:cut], par_2.bin_y[cut:]]
+        child_1_bin = [par_1_bin_x[0]+par_2_bin_x[1], par_1_bin_y[0]+par_2_bin_y[1]]
+        child_2_bin = [par_2_bin_x[0]+par_1_bin_x[1], par_2_bin_y[0]+par_1_bin_y[1]]
+        print(child_1_bin)
+
+        child_1_bin = [mutation(child_1_bin[0], self.mutation_rate), mutation(child_1_bin[1], self.mutation_rate)]
+        child_2_bin = [mutation(child_2_bin[0], self.mutation_rate), mutation(child_2_bin[1], self.mutation_rate)]
+
+        return child_1_bin, child_2_bin
+
+    def tournament(self, sample_size):
+        sample = random.sample(self.individuals, sample_size)
+        print(sample)
+    
+    #def initNewGeneration(self):
+
+            
 
 def fitFunc(x, y):
     return -(np.power(x, 2) + np.power(y, 2)) + 4
@@ -75,5 +129,5 @@ def realToBinary(realNum):
 
     return signalBit+binaryNum
 
-a = population(10)
+a = population(100, 1, 0.05)
 a.plotGraph()
