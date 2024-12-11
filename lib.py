5c96e387206759cce3748587521a72a949c38ef9
hidekekw <caioshimohiro@hotmail.com>
2024-12-11 14:29:21 -0300

    Completo

--- lib.py
+++ lib.py
@@ -11,18 +11,23 @@ class individual:
         self.fit = fitFunc(self.real_x, self.real_y)
 
 class population:
-    def __init__(self, pop_size, cross_rate, mutation_rate):
+    def __init__(self, pop_size, cross_rate, mutation_rate, elite):
         self.pop_size = pop_size
         self.generation = 1
         self.mutation_rate = mutation_rate*100 # Para realizar operacoes com inteiros ao inves de float
         self.cross_rate = cross_rate*100       # Idem
+        self.elite_pop = int(elite*pop_size)
         self.individuals = self.initFirstGen()
+        self.average_fit = -200
     
     def initFirstGen(self):
         pop = []
+        fit_sum = 0
         for i in range(self.pop_size):
             pop.append(individual(random.randint(-10000, 10000)/1000, random.randint(-10000, 10000)/1000))
             #print(pop[i].real_x, pop[i].real_y, pop[i].fit)
+            fit_sum += pop[i].fit
+        self.average_fit = fit_sum/self.pop_size
         return pop
     
     def plotGraph(self):
@@ -33,12 +38,12 @@ class population:
         y = []
         z = []
 
-        for individual in self.individuals:
-            x.append(individual.real_x)
-            y.append(individual.real_y)
+        for ind in self.individuals:
+            x.append(ind.real_x)
+            y.append(ind.real_y)
         z = fitFunc(x, y)
 
-        ax.scatter(x, y, z, color='red', s=10, label='Points')
+        ax.scatter(x, y, z, color='red', s=10, label='Points', alpha=0.01)
 
         ax.set_xlabel('Gene X')
         ax.set_ylabel('Gene Y')
@@ -53,8 +58,19 @@ class population:
 
         filename = "./graficos/gen"+str(self.generation)+".png"
         plt.savefig(filename)
+        plt.close(fig)
 
-    def cross(self, par_1_index, par_2_index):
+    def displayResults(self):
+        self.plotGraph()
+        filename = "./fitness/"+str(self.generation)+"gen.txt"
+        data = self.average_fit
+
+        # Open the file in write mode ('w')
+        with open(filename, "w") as file:
+            file.write(str(data))
+
+
+    def cross(self, par_1, par_2): # Cruzamento por 1 ponto aleatorio
 
         def bound_check(bin_gene):
             real_gene = binaryToReal(bin_gene)
@@ -79,11 +95,8 @@ class population:
             else:
                 return mut_gene_bin[0]+"10011100010000"
 
-        par_1 = self.individuals[par_1_index]
-        par_2 = self.individuals[par_2_index]
-
         if random.randint(0, 100) >= self.cross_rate: # Define se o cruzamento ocorre. Se nao, retorna os pais
-            return par_1, par_2
+            return [par_1.real_x, par_1.real_y], [par_2.real_x, par_2.real_y]
 
         cut = random.randint(1, 13)
         par_1_bin_x = [par_1.bin_x[:cut], par_1.bin_x[cut:]]
@@ -92,19 +105,70 @@ class population:
         par_2_bin_y = [par_2.bin_y[:cut], par_2.bin_y[cut:]]
         child_1_bin = [par_1_bin_x[0]+par_2_bin_x[1], par_1_bin_y[0]+par_2_bin_y[1]]
         child_2_bin = [par_2_bin_x[0]+par_1_bin_x[1], par_2_bin_y[0]+par_1_bin_y[1]]
-        print(child_1_bin)
 
         child_1_bin = [mutation(child_1_bin[0], self.mutation_rate), mutation(child_1_bin[1], self.mutation_rate)]
         child_2_bin = [mutation(child_2_bin[0], self.mutation_rate), mutation(child_2_bin[1], self.mutation_rate)]
 
-        return child_1_bin, child_2_bin
+        return [binaryToReal(child_1_bin[0]), binaryToReal(child_1_bin[1])], [binaryToReal(child_2_bin[0]), binaryToReal(child_2_bin[1])]
 
-    def tournament(self, sample_size):
+    def sampling(self, sample_size): # Selecao por torneio
+
+        def select_ind(sample):
+            max_fit = -200
+            max_fit_index = 0
+            for i, ind in enumerate(sample):
+                if ind.fit > max_fit:
+                    max_fit = ind.fit
+                    max_fit_index = i
+            return sample[max_fit_index]
+
         sample = random.sample(self.individuals, sample_size)
-        print(sample)
-    
-    #def initNewGeneration(self):
 
+        par_1 = select_ind(sample[:int(sample_size/2)])
+        par_2 = select_ind(sample[int(sample_size/2):])
+
+        return par_1, par_2
+    
+    def initNewGeneration(self, sample_size):
+        #print(len(self.individuals))
+
+        def eliteSelect():
+            elite_index = []
+            for i in range(self.elite_pop):
+                max_fit = -200
+                index = 0
+                for j, ind in enumerate(self.individuals):
+                    if ind.fit > max_fit and j not in elite_index: 
+                        max_fit = ind.fit
+                        index = j
+                elite_index.append(index)
+            return elite_index
+
+        new_gen = []
+
+        elite_index = eliteSelect()
+        for i in elite_index:
+            new_gen.append(self.individuals[i])
+
+        for i in range(self.elite_pop, self.pop_size):
+            par_1, par_2 = self.sampling(sample_size)
+            child_1_genes, child_2_genes = self.cross(par_1, par_2)
+            #print(child_1_genes, child_2_genes)
+            child_1 = individual(child_1_genes[0], child_1_genes[1])
+            child_2 = individual(child_2_genes[0], child_2_genes[1])
+
+            new_gen.append(child_1)
+            if len(new_gen) == self.pop_size:
+                break
+            new_gen.append(child_2)
+
+        fit_sum = 0
+        for ind in new_gen:
+            fit_sum += ind.fit
+
+        self.average_fit = fit_sum/self.pop_size
+        self.individuals = new_gen
+        self.generation += 1
             
 
 def fitFunc(x, y):
@@ -129,5 +193,9 @@ def realToBinary(realNum):
 
     return signalBit+binaryNum
 
-a = population(100, 1, 0.05)
-a.plotGraph()
+a = population(5000, 0.8, 0.01, 0.01)
+a.initFirstGen()
+a.displayResults()
+for i in range(20):
+    a.displayResults()
+    a.initNewGeneration(1000)
--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import lib
+fits = [-102, -4, -189, -20.5, 0.1, 0.5, -190]
+new_gen = []
 
-population = np.array([])
-x = 4
-y = -3.4
-z = lib.fitFunc(x, y)
+elite_index = []
+for i in range(3):
+    max_fit = -200
+    index = 0
+    for j, ind in enumerate(fits):
+        if ind > max_fit and j not in elite_index: 
+            max_fit = ind
+            index = j
+    elite_index.append(index)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-ax.scatter(x, y, z, color='red', s=10, label='Points')
-
-ax.set_xlabel('X Axis')
-ax.set_ylabel('Y Axis')
-ax.set_zlabel('Z Axis')
-ax.set_title('3D Scatter Plot')
-
-ax.legend()
-plt.show()
+print(elite_index)            
+new_gen.append(max_fit)
+fits = new_gen
